86f1cb5787b35498fe213af46124005f191af9d4
Acurich <91753821+Acurich@users.noreply.github.com>
2021-11-23 23:46:25 +0300

    Update IEEE-754 Converter.cpp

--- IEEE-754 Converter.cpp
+++ IEEE-754 Converter.cpp
@@ -1,73 +1,190 @@
 #include <iostream> 
-#include <string> 
-using namespace std; 
- 
-char to_String(double ost); 
-string Bin(int num); 
-string bins(double nums); 
- 
-string bins(double nums) { 
-int num = (int)nums; 
- double ost = nums-(double)num; 
- string bin_ost = ""; 
- int mas[100]; 
- int n = 0; 
- while (num > 0) { 
- mas[n]=num%2; 
- num/=2; 
- n++; 
- } 
- while (ost != 0) { 
- ost*=2; 
- bin_ost+=to_String(ost); 
- ost-=(int)ost; 
- } 
- string final = ""; 
- for (int i = 0; i < n; ++i) { 
- final+=to_String(mas[n-1-i]); 
- } 
- final=final+'.'+bin_ost; 
- return final; 
-} 
- 
-char to_String(double ost) { 
-return char((int)ost%10+48); 
-} 
- 
-string Bin(int num) { 
- string str = "", fin=""; 
- 
- while (num > 0) { 
- str+=to_String(num%2); 
- num/=2; 
- } 
- for (int i = 0; i < str.length(); ++i) fin+=str[str.length()-1-i]; 
- while (fin.length() != 8) fin= '0'+fin; 
- return fin; 
-} 
- 
-int main() { 
- long double nums; 
- cin >> nums; 
- int bi = 0; 
- if (nums < 0) bi = 1; 
- 
- string bin = bins(nums), mas[100], mantiss, por; 
- int n = 0; 
- 
- for (int i = 0; i < bin.length(); ++i) { 
- if (bin[i] == '.') { 
- for (int j = i; j > 1; j--) { 
- swap(bin[j-1], bin[j]); 
- n++; 
- } 
- } 
- } 
- 
- por = Bin(127+n); 
- for (int i = 2; i < 25; ++i) { 
- mantiss+=(char)bin[i]; 
- } 
- cout << bi << por << mantiss; 
-return 0; 
+#include <string>
+using namespace std;
+unsigned long long itc_len(string number) {
+	unsigned long long razmer = 0;
+	while (number[razmer] != '\0')razmer++;
+	return razmer;
+}
+int itc_size(string number) 
+{
+	int g = 0;
+	while (number[g] != '\0')
+		g++;
+	return g;
+}
+int itc_len_num(long long acur)
+{
+    int kr=0;
+    if(acur<0)
+    {
+        acur=acur*(-1);
+    }
+    if(acur==0)
+    {
+        return 1;
+    }
+    while(acur>0)
+    {
+        kr=kr+1;
+        acur=acur/10;
+    }
+    return (int(kr));
+}
+
+string itc_reverse_str(string number){
+    string result = "";
+    for (int i = 0; i < itc_len(number); i++){ result += number[itc_len(number)-i-1]; }
+    return result;
+}
+int expon(string number) 
+{
+	int dotoch = 0, posl = 0, count = 0, i = 0;
+	int razmer = itc_size(number);
+	while (i < razmer) // ищем "."
+	{
+		if (number[i] == '.')
+		{
+			dotoch = i;
+			break;
+		}
+		i++;
+	}
+	i = 0;
+	while (i < razmer) 
+	{
+		if (number[i] != '.')
+		{
+			if (number[i] == '1')
+			{
+				posl = count + 1; 
+				break;
+			}
+			count++;
+		}
+		i++;
+	}
+	return dotoch - posl;
+}
+string itc_toUpper(string number){
+    for(long long i = 0; i < itc_len(number); i++){
+        if(number[i] >= 'a' && number[i] <= 'z') number[i] -= 32;
+    }
+    return number;
+}
+
+unsigned long long itc_toInt(string number){
+    long long newnumber = 0;
+    for(long long i = 0; i < itc_len(number); i++){
+        newnumber += number[i] - 48;
+        newnumber *= 10;
+    }
+    newnumber /= 10;
+    return newnumber;
+}
+
+string itc_slice_str(string number, int nachalo, int konec){
+    long long dlina = itc_len(number);
+    string result;
+    if((konec > dlina) and (nachalo < dlina)){
+        for(int i = nachalo; i < dlina; i++){
+            result = result + number[i];
+        }
+        return result;
+    }
+    if(nachalo > konec){
+        return number;
+    }
+    for(int i = nachalo; i <= konec; i++){
+        result = result + number[i];
+    }
+    return result;
+}
+
+string itc_toString(unsigned long long number){
+    string newnumber = "";
+    string answer = "";
+    if(number == 0) return "0";
+    while(number > 0){
+        newnumber += number % 10 + 48;
+        number /= 10;
+    }
+    for(long long i = itc_len(newnumber) - 1; i >=0; i--){
+        answer += newnumber[i];
+    }
+    return answer;
+}
+
+long long bin_num_celliy(long long number){
+    int chislo;
+    long long result = 0, newnumber = 0, dlina = 0;
+    while(number > 0){
+        chislo = number % 2;
+        newnumber = newnumber * 10 + chislo;
+        number = number / 2;
+        dlina++;
+    }
+    while(dlina > 0){
+        chislo = newnumber % 10;
+        result = result * 10 + chislo;
+        newnumber = newnumber / 10;
+        dlina--;
+    }
+    return result;
+}
+
+string mantissa(string number) 
+{
+	string result = "";
+	int i = 0, count = 0;
+	bool flag = false;
+	while (i < itc_size(number) && count < 23)
+	{
+		if (flag)
+		{
+			if (number[i] != '.')
+			{
+				result += number[i];
+				count++;
+			}
+		}
+		if (number[i] == '1') 
+			flag = true;
+		i++;
+	}
+	while (count < 23) 
+	{
+		result += "0";
+		count++;
+	}
+	return result;
+}
+
+string bindrob(string number){
+    string chislo = "";
+    int dlina = itc_len(number);
+    unsigned long long newnumber = itc_toInt(number);
+    unsigned long long por = 1;
+    for (long long i = 0;i < dlina; i++){
+    por *= 10;
+    }
+    for(int j = 0; j <64; j++){
+        newnumber *= 2;
+        if (itc_len_num(newnumber) > dlina){
+        chislo += "1";
+        newnumber %= por;
+
+        }
+        else
+        chislo += "0";
+}
+    return chislo;
+}
+
+
+
+int main() {
+    string number;
+    cin >> number;
+    cout << expon(number);
 }
